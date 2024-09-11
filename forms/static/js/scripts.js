86cb5be1b890c8c44400563481ba78d990ed375b
R <93936601+rodrigocbsa@users.noreply.github.com>
2024-09-10 23:09:25 -0300

    pesquisa/ e correcoes gerais
    Falta p/ a demonstração do MVP: página de resultado!!!

--- forms/static/js/scripts.js
+++ forms/static/js/scripts.js
@@ -1,73 +1,41 @@
 $(function () {
+  // init tooltip & popovers
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  });
 
-    // init feather icons
-    feather.replace();
 
-    // init tooltip & popovers
-    $('[data-toggle="tooltip"]').tooltip();
-    $('[data-toggle="popover"]').popover();
+  // scroll top top
+  var scrollTop = 0;
+  $(window).scroll(function () {
+      var scroll = $(window).scrollTop();
+      
+      if (scroll > 80) {
+          if (scroll > scrollTop) {
+              $('.smart-scroll').addClass('scrolling').removeClass('up');
+          } else {
+              $('.smart-scroll').addClass('up');
+          }
+      } else {
+          // remove if scroll = scrollTop
+          $('.smart-scroll').removeClass('scrolling').removeClass('up');
+      }
 
-    //page scroll
-    $('a.page-scroll').bind('click', function (event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top - 20
-        }, 1000);
-        event.preventDefault();
-    });
+      scrollTop = scroll;
 
-    // slick slider
-    $('.slick-about').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 3000,
-        dots: true,
-        arrows: false
-    });
+      
+      if (scroll >= 600) {
+          $('.scroll-top').addClass('active');
+      } else {
+          $('.scroll-top').removeClass('active');
+      }
+      return false;
+  });
 
-    //toggle scroll menu
-    var scrollTop = 0;
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        //adjust menu background
-        if (scroll > 80) {
-            if (scroll > scrollTop) {
-                $('.smart-scroll').addClass('scrolling').removeClass('up');
-            } else {
-                $('.smart-scroll').addClass('up');
-            }
-        } else {
-            // remove if scroll = scrollTop
-            $('.smart-scroll').removeClass('scrolling').removeClass('up');
-        }
-
-        scrollTop = scroll;
-
-        // adjust scroll to top
-        if (scroll >= 600) {
-            $('.scroll-top').addClass('active');
-        } else {
-            $('.scroll-top').removeClass('active');
-        }
-        return false;
-    });
-
-    // scroll top top
-    $('.scroll-top').click(function () {
-        $('html, body').stop().animate({
-            scrollTop: 0
-        }, 1000);
-    });
-
-    /**Theme switcher - DEMO PURPOSE ONLY */
-    $('.switcher-trigger').click(function () {
-        $('.switcher-wrap').toggleClass('active');
-    });
-    $('.color-switcher ul li').click(function () {
-        var color = $(this).attr('data-color');
-        $('#theme-color').attr("href", "css/" + color + ".css");
-        $('.color-switcher ul li').removeClass('active');
-        $(this).addClass('active');
-    });
-});
+  $('.scroll-top').click(function () {
+      $('html, body').stop().animate({
+          scrollTop: 0
+      }, 1000);
+  });
+})
