486f954f7139496c33941c8c862cfb655ddd5806
R <93936601+rodrigocbsa@users.noreply.github.com>
2024-09-18 13:45:26 -0300

    bd muito simples

--- forms/admin.py
+++ forms/admin.py
@@ -1,1 +1,12 @@
 from django.contrib import admin
+from .models import Pergunta,Opcoes
+
+class OpcoesInline(admin.TabularInline):
+    model = Opcoes
+    extra = 5
+
+class PerguntaAdmin(admin.ModelAdmin):
+    inlines = [OpcoesInline]
+
+# Registro no Django Admin
+admin.site.register(Pergunta,PerguntaAdmin)
--- forms/models.py
+++ forms/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+class Pergunta(models.Model):
+    titulo = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.titulo
+
+class Opcoes(models.Model):
+    pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+    opcao = models.CharField(max_length=30)
+    votos = models.IntegerField(default=0)
