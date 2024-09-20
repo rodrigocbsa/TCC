8979e8d7f42ab46415a96306ed99c5a911ca640c
R <93936601+rodrigocbsa@users.noreply.github.com>
2024-09-19 22:44:02 -0300

    TAREFA 2 (save point)
    corrigir 5opcoes Pergunta
    cadastrar para teste

--- forms/admin.py
+++ forms/admin.py
@@ -1,13 +1,6 @@
 from django.contrib import admin
-from .models import Pergunta,Opcoes
+from .models import Opcoes,Pergunta,Parametro,Indicador,Categoria,Dimensao,Subdimensao
 
-class OpcoesInline(admin.TabularInline):
-    model = Opcoes
-    extra = 5
-    readonly_fields = ('votos',)
-
-class PerguntaAdmin(admin.ModelAdmin):
-    inlines = [OpcoesInline]
 
 # Registro no Django Admin
-admin.site.register(Pergunta,PerguntaAdmin)
+admin.site.register([Opcoes,Pergunta,Parametro,Indicador,Categoria,Dimensao,Subdimensao])
