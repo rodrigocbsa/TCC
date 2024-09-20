cbefa19f478bde6df7e62bcbde797e28beb95bd5
R <93936601+rodrigocbsa@users.noreply.github.com>
2024-09-19 22:52:06 -0300

    TAREFA 2 OK
    1_ trocar bd para MYSQL......... OK
    2_ modelar relações e cadastrar entidades OK
    3_ graph.js DOING
    4_ survey template UNDONE

--- forms/admin.py
+++ forms/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Opcoes,Pergunta,Parametro,Indicador,Categoria,Dimensao,Subdimensao
 
+class OpcoesInline(admin.TabularInline):
+    model = Opcoes
+    extra = 5
+    readonly_fields = ('votos',)
+
+class PerguntaAdmin(admin.ModelAdmin):
+    inlines = [OpcoesInline]
 
 # Registro no Django Admin
-admin.site.register([Opcoes,Pergunta,Parametro,Indicador,Categoria,Dimensao,Subdimensao])
+admin.site.register(Pergunta,PerguntaAdmin)
+admin.site.register([Parametro,Indicador,Categoria,Dimensao,Subdimensao])
--- forms/models.py
+++ forms/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 #'''
+class Pergunta(models.Model):
+    titulo = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.titulo
+    
 class Opcoes(models.Model):
+    pergunta = models.ForeignKey(Pergunta, related_name='opcoes', on_delete=models.CASCADE, null=True) # related_name para acessar a referência nas tags
     opcao = models.CharField(max_length=30)
     votos = models.IntegerField(default=0)
 
     def __str__(self):
         return self.opcao
-
-class Pergunta(models.Model):
-    opcoes = models.ForeignKey(Opcoes, related_name='opcoes', on_delete=models.CASCADE) # related_name para acessar a referência nas tags
-    titulo = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.titulo
 
 class Parametro(models.Model):
     pergunta = models.ForeignKey(Pergunta, related_name='perguntas', on_delete=models.CASCADE)
